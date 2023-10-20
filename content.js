649b4ffd8309043e42e0e0c8a1728746bb783641
Abhinav Aditya <abhinavaditya162@gmail.com>
2023-10-20 10:10:08 +0530

    stop function fixes

--- content.js
+++ content.js
@@ -2,38 +2,39 @@ let timeOut1 = null;
 let timeOut2 = null;
 let timeOut3 = null;
 let timeOut4 = null;
+let interval1 = null;
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.msg === "start") {
-        setInterval(() => {
+        interval1 = setInterval(() => {
             var wrapper = document.querySelector(`.app > div > :last-child > footer`)
 
             // var skipbtn = document.querySelector(`footer > div > div > div > button`);
-            setTimeout(() => {
+            timeOut1 = setTimeout(() => {
                 wrapper.querySelector(`footer > div > div > div > button`).click();
                 // skipbtn.click();
             }, 1000)
 
             // var quesSkipModal = document.querySelector(`#question-skip-modal > div > div > :first-child > :nth-child(2) > div > div > input`);
-            setTimeout(() => {
+            timeOut2 = setTimeout(() => {
                 document.querySelector(`#question-skip-modal > div > div > :first-child > :nth-child(2) > div > div > input`).click();
             }, 2000)
 
 
             // var donTkNOW = document.querySelector(`#skip_radio_button_container_4 > label`);
-            setTimeout(() => {
+            timeOut3 = setTimeout(() => {
                 document.querySelector(`#skip_radio_button_container_4 > label`).click();
                 // donTkNOW.click();
             }, 3000)
 
-            setTimeout(() => {
+            timeOut4 = setTimeout(() => {
                 document.querySelector(`#modal-submit-button`).click()
             }, 4000)
         }, 5000)
     }
-    else if (message.msg === "stop")
-    {
+    else if (message.msg === "stop") {
         console.log("Stop message received");
+        clearInterval(interval1);
         clearInterval(timeOut1);
         clearInterval(timeOut2);
         clearInterval(timeOut3);
